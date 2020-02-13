e97cc0999926edafe1553eb1de84d6b985633c60
Joe Tindall <joe.tindall@firemon.com>
2020-02-13 09:49:39 -0600

    Return file list for reference.

--- src/main/java/com/tenable/io/api/exports/ExportHelper.java
+++ src/main/java/com/tenable/io/api/exports/ExportHelper.java
@@ -1,19 +1,22 @@
 package com.tenable.io.api.exports;
 
+import java.io.File;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import com.tenable.io.api.TenableIoClient;
 import com.tenable.io.api.exports.models.AssetsExportRequest;
+import com.tenable.io.api.exports.models.ExportStatus;
 import com.tenable.io.api.exports.models.Status;
 import com.tenable.io.api.exports.models.VulnsExportRequest;
-import com.tenable.io.api.exports.models.ExportStatus;
 import com.tenable.io.core.exceptions.TenableIoException;
 
-import java.io.File;
-
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * Copyright (c) 2018 Tenable Network Security, Inc.
  */
+@Slf4j
 public class ExportHelper {
     private static final int sleepInterval = 5000;
     private TenableIoClient client;
@@ -36,24 +39,33 @@ public class ExportHelper {
      * @param downloadPath File Path to save the chunk downloads to
      * @param exportRequest The VulnsExportRequest to set filters
      * @throws TenableIoException
+     * @return
      */
-    public void downloadVulns( String downloadPath, VulnsExportRequest exportRequest  ) throws TenableIoException {
+    public List<File> downloadVulns( String downloadPath, VulnsExportRequest exportRequest  ) throws TenableIoException {
         String exportUuid = client.getExportsApi().vulnsRequestExport( exportRequest );
         ExportStatus status = this.client.getExportsApi().vulnsExportStatus( exportUuid );
         while( !status.getStatus().equals( Status.FINISHED ) ) {
             try {
                 Thread.sleep( sleepInterval );
             } catch( InterruptedException e ) {
+                log.error(e.getMessage());
             }
 
             status = this.client.getExportsApi().vulnsExportStatus( exportUuid );
         }
 
         // Download chunks
-        for ( int id : status.getChunksAvailable() ) {
-            File downloadFile = new File( downloadPath + "_" + id );
-            this.client.getExportsApi().vulnsDownloadChunk( exportUuid, id, downloadFile );
-        }
+        return status.getChunksAvailable().stream().map(chunkId -> {
+            File downloadFile = new File( downloadPath + "_" + chunkId);
+            try {
+                this.client.getExportsApi().vulnsDownloadChunk( exportUuid, chunkId, downloadFile );
+            } catch (TenableIoException e) {
+                // log the exception but keep processing
+                log.error(e.getMessage());
+            }
+            log.info("Saved TenableIO chunk to file {}", downloadFile.getPath());
+            return downloadFile;
+        }).collect(Collectors.toList());
 
     }
 
