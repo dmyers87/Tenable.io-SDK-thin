551f3aa671f18881aee8273f586e13c9bc709c81
Joe Tindall <joe.tindall@firemon.com>
2020-02-19 10:55:41 -0600

    Add session api

--- src/main/java/com/tenable/io/api/session/SessionHelper.java
+++ src/main/java/com/tenable/io/api/session/SessionHelper.java
@@ -3,7 +3,6 @@ package com.tenable.io.api.session;
 import com.tenable.io.api.ApiHelperBase;
 import com.tenable.io.api.TenableIoClient;
 import com.tenable.io.api.session.model.Session;
-import com.tenable.io.core.exceptions.TenableIoErrorCode;
 import com.tenable.io.core.exceptions.TenableIoException;
 
 import lombok.extern.slf4j.Slf4j;
@@ -23,10 +22,6 @@ public class SessionHelper extends ApiHelperBase {
   }
 
   public Session validate() throws TenableIoException {
-    Session activeSession = this.client.getSessionApi().getActiveSession();
-    if (activeSession == null || activeSession.getId() == null) {
-      throw new TenableIoException(TenableIoErrorCode.NotAuthorized, "No active authorized session");
-    }
-    return activeSession;
+    return this.client.getSessionApi().getActiveSession();
   }
 }
